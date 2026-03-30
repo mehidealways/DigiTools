bf2221c4301d8193d2bdfb022ffc2d5b0cedeb3d
mehidealways <mehidehasanmilon444@.com>
2026-03-30 23:25:01 +0600

    Initialize All Resorces

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+// const getInfo = async () => {
+//   const res = await fetch('/public/information.json');
+//   return res.json();
+// };
+// const infoPromise = getInfo();
+
+function App() {
+  return <>
+    <h1>Hello World What's up</h1>
+  </>;
+}
+
+export default App;
